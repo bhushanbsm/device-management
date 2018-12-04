a9068e6d86774c16f4d4cb40fa6c98a71992d760
Bhushan Manusmare <bhushanbsm@gmail.com>
2018-12-04 22:37:35 +0530

    Feature/integrate theme (#1)
    * added admin theme and created partials
    
    * changed copyright from footer

---
+++ application/views/partials/footer.php
@@ -0,0 +1,21 @@
+   </div>
+   <hr>
+   <footer>
+    <p>&copy; Bhushan Manusmare 2018</p>
+</footer>
+</div>
+<span id="base_url" class="hidden"><?=base_url()?></span>
+<!--/.fluid-container-->
+<script src="<?=base_url()?>assets/admin-theme/vendors/jquery-1.9.1.min.js"></script>
+<script src="<?=base_url()?>assets/admin-theme/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?=base_url()?>assets/admin-theme/vendors/easypiechart/jquery.easy-pie-chart.js"></script>
+<script src="<?=base_url()?>assets/admin-theme/assets/scripts.js"></script>
+<script>
+    $(function() {
+            // Easy pie charts
+            $('.chart').easyPieChart({animate: 1000});
+        });
+    </script>
+</body>
+
+</html>
